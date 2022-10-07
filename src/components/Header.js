a512d122d76c7b9774907dbb6b541c594d16e7a6
Larppa1 <lari.juva7@gmail.com>
2022-10-07 10:38:16 +0300

    Header.js: Code readability improvements.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,24 @@ export default function Header() {
         <div className="container">
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
-                    <a className="btn btn-ghost normal-case text-xl" href="/">Social App</a>
+                    <a className="btn btn-ghost normal-case text-xl"
+                        href="/">
+                        Social App
+                    </a>
                 </div>
-                <div className="navbar-center">
-                    
-                </div>
+                <div className="navbar-center"></div>
                 <div className="navbar-end">
                     <button className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        <svg className="h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                            />
+                        </svg>
                     </button>
                 </div>
             </div>

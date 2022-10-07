6aaec69ca194ae27dd71f38eb6f409231db79b60
Larppa1 <lari.juva7@gmail.com>
2022-10-07 11:12:34 +0300

    Section.js: Add 'p' to sectionContainer.

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -3,7 +3,7 @@ import './sectionStyles.css'
 export default function Section() {
     return(
         <div className='sectionContainer'>
-            
+            <p>Hello</p>
         </div>
     )
 }

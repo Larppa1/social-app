128d2b6f4d5c5a3ea5f82360c30a2cd4701bbf34
Larppa1 <lari.juva7@gmail.com>
2022-10-08 11:56:07 +0300

    Section.js: Add div.

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -3,7 +3,9 @@ import './sectionStyles.css'
 export default function Section() {
     return(
         <div className='sectionContainer shadow'>
-            
+            <div>
+                
+            </div>
         </div>
     )
 }

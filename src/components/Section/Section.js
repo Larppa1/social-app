c73ba6d98ab61348f62c2450132d0a85d80ca3c1
Larppa1 <lari.juva7@gmail.com>
2022-10-08 11:17:04 +0300

    Section.js: Add placeholder p elements to sectionContainer.

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -4,6 +4,18 @@ export default function Section() {
     return(
         <div className='sectionContainer'>
             <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
         </div>
     )
 }

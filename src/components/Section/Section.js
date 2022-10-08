893b7a382efa0946c5a593504e4e33353b73fb2e
Larppa1 <lari.juva7@gmail.com>
2022-10-08 12:01:14 +0300

    .

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -4,7 +4,7 @@ export default function Section(props) {
     return(
         <div className='sectionContainer shadow'>
             <div>
-                <h4>{props.title}</h4>
+                <h4>{props.title}</h4> 
             </div>
         </div>
     )

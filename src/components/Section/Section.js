234f09d6516add7fbc35041bdfd17a70bcf620a6
Larppa1 <lari.juva7@gmail.com>
2022-10-08 11:55:07 +0300

    Section.js: Remove placeholder p elements.

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -3,19 +3,7 @@ import './sectionStyles.css'
 export default function Section() {
     return(
         <div className='sectionContainer shadow'>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
+            
         </div>
     )
 }

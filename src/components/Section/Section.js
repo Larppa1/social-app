40a9919224685de7ad6785b168ef721857220747
Larppa1 <lari.juva7@gmail.com>
2022-10-08 11:56:26 +0300

    Section.js: Add h4.

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -4,7 +4,7 @@ export default function Section() {
     return(
         <div className='sectionContainer shadow'>
             <div>
-                
+                <h4>{this.props.title}</h4>
             </div>
         </div>
     )

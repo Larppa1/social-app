f86c63ea43467b96f17791e4c4774f904358d9dd
Larppa1 <lari.juva7@gmail.com>
2022-10-09 11:40:50 +0300

    Home.js: Remove Content.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,12 @@
 import Header from "../components/Header/Header"
 import Section from '../components/Section/Section'
-import Content from '../components/Content/Content'
 
 export default function Home() {
     return(
         <div className="homeContainer">
             <Header />
             <div className="contentContainer">
-                <Section title="Trending">
-                    <Content />
-                </Section>
+                <Section title="Trending" />
                 <Section title="Recommended" />
                 <Section title="Friends" />
             </div>

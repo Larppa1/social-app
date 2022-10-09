398a8975dabc30c95980f8667ea3ae6585bb43e6
Larppa1 <lari.juva7@gmail.com>
2022-10-09 11:31:26 +0300

    Home.js: Import Content component.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import Header from "../components/Header/Header"
 import Section from '../components/Section/Section'
+import Content from '../components/Content/Content'
 
 export default function Home() {
     return(
